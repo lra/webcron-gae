490f29738cd18e4f3d22b949a482d7b209c92095
Laurent Raufaste <laurent@percolate.com>
2012-05-27 21:18:32 -0400

    1st running cron

---
+++ playlist.py
@@ -0,0 +1,5 @@
+import webcron
+
+
+url = 'http://glop.herokuapp.com/playlist/update.php'
+webcron.get_url_and_send_output(url)
---
+++ webcron.py
@@ -0,0 +1,29 @@
+from google.appengine.api import urlfetch
+from google.appengine.api import mail
+
+
+def output_http(message):
+    print 'Content-Type: text/plain'
+    print ''
+    print message
+
+
+def mail_text(message):
+    sender = "update-playlist@webcron-gae.appspotmail.com"
+    subject = "Trouble while updating the FG playlist DB"
+    mail.send_mail_to_admins(sender, subject, message)
+
+
+def output_and_email(message):
+    mail_text(message)
+    output_http(message)
+
+
+def get_url_and_send_output(url):
+    result = urlfetch.fetch(url)
+    if result.status_code == 200:
+        if result.content:
+            output_and_email(result.content)
+    else:
+        message = 'Could not get {} =( Error '.format(url, result.status_code)
+        output_and_email(message)
